75e39a03033b71c752496f828c2ca9e1fc2e234e
HuaVanAn <37148791+HuaVanAn@users.noreply.github.com>
2018-03-10 09:53:43 +0700

    Add files via upload

---
+++ baitapso44.java
@@ -0,0 +1,21 @@
+package baitaptieptheo;
+
+import java.util.Scanner;
+
+public class baitapso44 {
+
+	public baitapso44() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int a;
+  char s1,s2,s3;
+  Scanner key = new Scanner(System.in);
+  System.out.print("Nhap a: ");
+  a = key.nextInt();
+  System.out.printf("%d + %d%d + %d%d%d\n",a,a,a,a,a,a);
+	}
+
+}
