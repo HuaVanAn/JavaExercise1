d60095adbbae4e67b6336ac6da44071b438d394f
HuaVanAn <37148791+HuaVanAn@users.noreply.github.com>
2018-03-10 09:54:26 +0700

    Add files via upload

---
+++ baitapso48.java
@@ -0,0 +1,20 @@
+package baitaptieptheo;
+
+public class baitapso48 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Các số lẻ từ 1 đến 100 la:\n ");
+		for (int i=1; i<100; i++) {
+			if (i%2!=0) {
+				System.out.print(i);
+				System.out.printf("\n");
+			}
+		
+		}
+	
+	
+	}
+}
+
+
