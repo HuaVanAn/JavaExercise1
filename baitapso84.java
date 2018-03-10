cd0cdcbaecdbb0ffa3b381a5e2fa84018c3192ac
HuaVanAn <37148791+HuaVanAn@users.noreply.github.com>
2018-03-10 21:14:04 +0700

    Add files via upload

---
+++ baitapso84.java
@@ -0,0 +1,17 @@
+package baitaptieptheo;
+
+import java.util.*; 
+import java.io.*; 
+ public class baitapso84 {
+ public static void main(String[] args)
+ {
+   String string1 = "Python";
+    int slength = 3;
+    if (slength > string1.length()) {
+      slength = string1.length();
+    }
+
+    String subpart = string1.substring(string1.length()-3);
+    System.out.println(subpart + string1 + subpart);
+  }
+}
